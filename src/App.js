8dae5664df53a732625218863dc0ab022cf4de39
JoeLuciano <joeluciano@outlook.com>
2022-05-10 00:31:24 -0400

    Created impactibles protoype template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,136 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion';
+
+const symbolContainer = {
+  base: { scale: 1, backgroundColor: '#1a1' },
+  focus: { scale: 1.1, backgroundColor: 'var(--light-background)' },
+  selected: { scale: 1, backgroundColor: 'var(--light-background)' },
+};
+
+const symbolSvgContainer = {
+  base: { backgroundColor: 'var(--summary-background)' },
+  focus: {},
+  selected: { backgroundColor: 'var(--summary-background)' },
+};
+
+const symbolSummary = {
+  base: {},
+  focus: {},
+  selected: { backgroundColor: 'var(--summary-background)' },
+};
+
+const svg = {
+  hidden: {
+    scale: 1,
+  },
+  visible: {
+    scale: 1.5,
+  },
+};
+
+const Svg = () => {
+  return (
+    <motion.div
+      layoutId='svg'
+      className='svg'
+      variants={svg}
+      initial='hidden'
+      animate='visible'></motion.div>
+  );
+};
 
 function App() {
+  const symbolContainerControls = useAnimation();
+  const [pageState, setPageState] = useState('base');
+
+  useEffect(() => {
+    symbolContainerControls.set('base');
+  }, [symbolContainerControls]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <motion.div
+        className='symbolContainer'
+        variants={symbolContainer}
+        animate={symbolContainerControls}
+        onHoverStart={() => {
+          setPageState((prev) => {
+            if (prev === 'base') {
+              symbolContainerControls.start('focus');
+              return 'focus';
+            }
+            return prev;
+          });
+        }}
+        onHoverEnd={() => {
+          setPageState((prev) => {
+            if (prev === 'focus') {
+              symbolContainerControls.start('base');
+              return 'base';
+            }
+            return prev;
+          });
+        }}>
+        {pageState !== 'selected' && (
+          <motion.div
+            layoutId='symbolBackground'
+            className='symbolSvgContainer'
+            variants={symbolSvgContainer}
+            initial='base'
+            animate='selected'
+            onTap={() => {
+              setPageState((prev) => {
+                if (prev === 'focus') {
+                  symbolContainerControls.start('selected');
+                  return 'selected';
+                } else {
+                  symbolContainerControls.start('base');
+                  return 'base';
+                }
+              });
+            }}>
+            <Svg />
+          </motion.div>
+        )}
+        {pageState === 'selected' && (
+          <motion.div
+            layoutId='symbolBackground'
+            className='symbolSummary'
+            variants={symbolSummary}
+            initial='base'
+            animate='selected'>
+            <motion.svg
+              className='symbolSummaryBorder'
+              height='100%'
+              width='100%'>
+              <motion.rect
+                height='100%'
+                width='100%'
+                strokeWidth={10}
+                stroke='var(--white)'
+                fill='var(--transparent)'
+              />
+            </motion.svg>
+            <motion.div
+              className='toggleFocusButton'
+              onTap={() => {
+                setPageState((prev) => {
+                  if (prev === 'focus') {
+                    symbolContainerControls.start('selected');
+                    return 'selected';
+                  } else {
+                    symbolContainerControls.start('base');
+                    return 'base';
+                  }
+                });
+              }}>
+              BACK
+            </motion.div>
+            <Svg />
+          </motion.div>
+        )}
+      </motion.div>
     </div>
   );
 }
